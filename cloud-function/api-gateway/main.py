ed34e824b36bcfa874783433367285e84fada4ec
Russ Wirtz <russ@Russs-MacBook-Pro.local>
2023-01-12 11:08:06 -0800

    updates for api routing

--- cloud-function/api-gateway/main.py
+++ cloud-function/api-gateway/main.py
@@ -1,44 +1,45 @@
-from flask import jsonify, Response
-import time
+from flask import Response
 import json
 from google.cloud import workflows_v1beta
 from google.cloud.workflows import executions_v1beta
 from google.cloud.workflows.executions_v1beta.types import executions
 
-def api_gateway(request) -> Response:
-    project = 'spanner-demo-1'
-    location = 'us-west2'
-    workflow = 'adjustmentsWorkflow'
-    return execute_workflow(request, project, location, workflow)
+IP_ADDRESS_HEADER: str = 'X-Forwarded-For'
+ORIGINAL_PATH_HEADER: str = 'X-Envoy-Original-Path'
 
-def execute_workflow(request, project, location, workflow):
+# TODO: import via config
+GCP_PROJECT: str = 'spanner-demo-369019'
+GCP_REGION: str = 'us-west2'
+
+def api_gateway(request) -> Response:
+    ip_address = request.headers.get(IP_ADDRESS_HEADER)
+    api_path = request.headers.get(ORIGINAL_PATH_HEADER).split("?")[0] if "?" in request.headers.get(ORIGINAL_PATH_HEADER) else request.headers.get(ORIGINAL_PATH_HEADER)
+    print(f"API request from IP: {ip_address} for path: {api_path}")
+
+    if ("/adjustments" == api_path):
+        workflow = 'adjustmentsWorkflow'
+    else:
+        raise Exception(f"Unknown API path: {api_path}")
+
+    return execute_workflow(request, GCP_PROJECT, GCP_REGION, workflow)
+
+def execute_workflow(request, project, location, workflow) -> Response:
     if not project:
         raise Exception('project is required.')
 
-    # Set up API clients.
     execution_client = executions_v1beta.ExecutionsClient()
     workflows_client = workflows_v1beta.WorkflowsClient()
-    print(f"Got api gateway request - {str(request.get_json())}")
 
     parent = workflows_client.workflow_path(project, location, workflow)
-    response = execution_client.create_execution(parent=parent, execution=executions.Execution(argument=json.dumps(request.get_json())))
-    print(f"Created execution: {response.name}")
 
-    execution_finished = False
-    backoff_delay = 0.1
-    print('Poll for result...')
-    while (not execution_finished):
-        execution = execution_client.get_execution(request={"name": response.name})
-        execution_finished = execution.state != executions.Execution.State.ACTIVE
+    response = execution_client.create_execution(
+        parent=parent, 
+        execution=executions.Execution(argument=json.dumps(request.get_json())))
+    
+    execution_id = response.name.split("/executions/")[1]
+    
+    print(f"Created execution id: {execution_id} with name {response.name}")
 
-        # If we haven't seen the result yet
-        if not execution_finished:
-            print('- Waiting for results...')
-            time.sleep(backoff_delay/10)
-            backoff_delay *= 2  # Double the delay to provide exponential backoff.
-        else:
-            print(f'Execution finished with state: {execution.state.name}')
-            print(execution.result)
-            return execution.result
+    return {"execution_id": execution_id}, 200
 
 
